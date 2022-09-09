95d59fd36fc1dcd186f24762849a1bb2988e06f9
Kevin Daudt <kdaudt@alpinelinux.org>
2022-09-09 23:26:37 +0200

    backend: allow any origin to connect to the websocket

--- backend/websocket.go
+++ backend/websocket.go
@@ -136,7 +136,7 @@ func (b *BuildStatusPublisher) PublishBuildStatus(ctx context.Context) {
 func (b *BuildStatusPublisher) ListenWebsocket(ctx context.Context) {
 	go b.PublishBuildStatus(ctx)
 
-	upgrader := websocket.Upgrader{}
+	upgrader := websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		conn, err := upgrader.Upgrade(w, r, nil)
 		if err != nil {
